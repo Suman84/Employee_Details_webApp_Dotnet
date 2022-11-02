9a26f7fc33335dfe4bceda84f9042c9b6428d062
sumankhanal770 <sumankhanal770@gmail.com>
2022-11-02 21:23:36 +0545

    implementing edit for employee history startdate and enddates

--- Employee_details_webapp/Employee_details_webapp/Controllers/CombinedController.cs
+++ Employee_details_webapp/Employee_details_webapp/Controllers/CombinedController.cs
@@ -11,7 +11,7 @@ using ServiceLayer.Services;
 namespace Employee_details_webapp.Controllers
 {
     public class CombinedController : Controller
-    { 
+    {
 
         private readonly IPositionService _positionService;
         private readonly IPeopleService _peopleService;
@@ -19,7 +19,7 @@ namespace Employee_details_webapp.Controllers
         private readonly IEmployeeJobHistoryService _employeeJobHistoryService;
         private IValidator<AddViewModel> _validator;
 
-        public CombinedController(IPositionService positionService, IPeopleService peopleService, IEmployeeService employeeService, 
+        public CombinedController(IPositionService positionService, IPeopleService peopleService, IEmployeeService employeeService,
             IEmployeeJobHistoryService employeeJobHistoryService, IValidator<AddViewModel> validator)
         {
             _positionService = positionService;
@@ -154,7 +154,6 @@ namespace Employee_details_webapp.Controllers
         [HttpPost]
         public IActionResult EditEmployee(EditViewModel editViewModel)
         {
-
             var people = new People()
             {
                 Personid = editViewModel.Personid,
@@ -164,9 +163,6 @@ namespace Employee_details_webapp.Controllers
                 Address = editViewModel.Address,
                 Email = editViewModel.Email
             };
-            _peopleService.UpdatePeople(people);
-
-
             var employee = new Employees()
             {
                 Employeeid = editViewModel.Employeeid,
@@ -177,18 +173,20 @@ namespace Employee_details_webapp.Controllers
                 Personid = editViewModel.Personid,
                 Positionid = editViewModel.Positionid,
             };
+            var employeeJobHistory = new EmployeeJobHistories()
+            {
+                EmployeeJobHistoryid = Guid.NewGuid(),
+                Employeeid = editViewModel.Employeeid,
+                Positionid = editViewModel.Positionid,
+                StartDate = DateOnly.FromDateTime(editViewModel.StartDate),
+                EndDate = DateOnly.FromDateTime(editViewModel.EndDate)
+            };
+
+            _peopleService.UpdatePeople(people);
             _employeeService.UpdateEmployee(employee);
 
-            if(editViewModel.Positionid != editViewModel.OriginalPositionid)
+            if (editViewModel.Positionid != editViewModel.OriginalPositionid)
             {
-                var employeeJobHistory = new EmployeeJobHistories()
-                {
-                    EmployeeJobHistoryid = Guid.NewGuid(),
-                    Employeeid = editViewModel.Employeeid,
-                    Positionid = editViewModel.Positionid,
-                    StartDate = DateOnly.FromDateTime(editViewModel.StartDate),
-                    EndDate = DateOnly.FromDateTime(editViewModel.EndDate)
-                };
                 _employeeJobHistoryService.InsertEmployeeJobHistory(employeeJobHistory);
             }
 
@@ -196,23 +194,22 @@ namespace Employee_details_webapp.Controllers
             return Redirect(Url.Action("AllEmployeesList", "Combined") + "");
         }
 
-
-        [HttpGet]
+        [HttpGet("/Combined/EmployeeJobHistoryList/{Id}")]
         public IActionResult EmployeeJobHistoryList(Guid Id)
         {
             var employee = _employeeService.GetEmployee(Id);
             var person = _peopleService.GetPeople(employee.Personid);
-            ViewBag.fullname = person.FirstName +" "+ person.MiddleName +" "+ person.LastName;
+            ViewBag.fullname = person.FirstName + " " + person.MiddleName + " " + person.LastName;
 
             var employeeJobHistoryList = new List<EmployeeJobHistoriesModel>();
             var tempEmployeeJobHistoryList = _employeeJobHistoryService.GetAllEmployeeJobHistories().ToList();
 
             tempEmployeeJobHistoryList.ForEach(employeeJobHistory =>
             {
-                if(employeeJobHistory.Employeeid == Id)
+                if (employeeJobHistory.Employeeid == Id)
                 {
                     var tempPosition = _positionService.GetPosition(employeeJobHistory.Positionid);
-                   
+
                     EmployeeJobHistoriesModel employeeJobHistoryModel = new()
                     {
                         EmployeeJobHistoryid = employeeJobHistory.EmployeeJobHistoryid,
@@ -230,5 +227,60 @@ namespace Employee_details_webapp.Controllers
 
         }
 
+        [HttpGet("/Combined/EmployeeJobHistoryEdit/{Id}/{Id2}")]
+        public IActionResult EmployeeJobHistoryEdit(Guid Id, Guid Id2)
+        {
+            var employee = _employeeService.GetEmployee(Id);
+            var person = _peopleService.GetPeople(employee.Personid);
+            var employeeJobHistory = _employeeJobHistoryService.GetEmployeeJobHistory(Id2);
+
+            ViewBag.fullname = person.FirstName + " " + person.MiddleName + " " + person.LastName;
+            ViewBag.jobHistory = employeeJobHistory;
+
+            var employeeJobHistoryList = new List<EmployeeJobHistoriesModel>();
+            var tempEmployeeJobHistoryList = _employeeJobHistoryService.GetAllEmployeeJobHistories().ToList();
+
+            tempEmployeeJobHistoryList.ForEach(employeeJobHistory =>
+            {
+                if (employeeJobHistory.Employeeid == Id)
+                {
+                    var tempPosition = _positionService.GetPosition(employeeJobHistory.Positionid);
+
+                    EmployeeJobHistoriesModel employeeJobHistoryModel = new()
+                    {
+                        EmployeeJobHistoryid = employeeJobHistory.EmployeeJobHistoryid,
+                        Employeeid = employeeJobHistory.Employeeid,
+                        Positionid = employeeJobHistory.Positionid,
+                        StartDate = employeeJobHistory.StartDate,
+                        EndDate = employeeJobHistory.EndDate,
+                        PositionName = tempPosition.PositionName,
+                    };
+
+                    employeeJobHistoryList.Add(employeeJobHistoryModel);
+                }
+            });
+
+            ViewBag.jobHistoryList = employeeJobHistoryList;
+
+            var tempPosition2 = _positionService.GetPosition(employeeJobHistory.Positionid);
+            EmployeeJobHistoriesModel employeeJobHistoryModel2 = new()
+            {
+                EmployeeJobHistoryid = employeeJobHistory.EmployeeJobHistoryid,
+                Employeeid = employeeJobHistory.Employeeid,
+                Positionid = employeeJobHistory.Positionid,
+                StartDate = employeeJobHistory.StartDate,
+                EndDate = employeeJobHistory.EndDate,
+                PositionName = tempPosition2.PositionName,
+            };
+
+            return View(employeeJobHistoryModel2);
+        }
+
+        [HttpPost("/Combined/EmployeeJobHistoryEdit/{Id}/{Id2}")]
+        public IActionResult EmployeeJobHistoryEdit(EmployeeJobHistoriesModel employeeJobHistory)
+        {
+            return RedirectToAction("../");
+            return Redirect(Url.Action("EmployeeJobHistoryList/{Id}", "Combined") + "");
+        }
     }
 }
